12892833f2dfcc859f0e5a8bb95e546e268dbb89
pawa <pawa@pawafuru.com>
2014-06-27 10:59:43 +0900

    Updated to Ver. 0.45

--- lib/Lingua/JA/NormalizeText.pm
+++ lib/Lingua/JA/NormalizeText.pm
@@ -14,7 +14,7 @@ use Lingua::JA::Regular::Unicode ();
 use Lingua::JA::Dakuon ();
 use Lingua::JA::Moji   ();
 
-our $VERSION   = '0.43';
+our $VERSION   = '0.45';
 our @EXPORT    = qw();
 our @EXPORT_OK = qw(nfkc nfkd nfc nfd decode_entities strip_html
 alnum_z2h alnum_h2z space_z2h space_h2z katakana_z2h katakana_h2z
